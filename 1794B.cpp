2243e5362a058fa9c9e2ded408f4d803e468f4aa
Kinjal2103 <kinjalagarwal2103@gmail.com>
2025-08-21 01:07:45 +0530

    Can be reattempted

---
+++ 1794B.cpp
@@ -0,0 +1,50 @@
+//https://codeforces.com/problemset/problem/1794/B
+#include <bits/stdc++.h>
+using namespace std;
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pb push_back
+#define pob pop_back
+#define fori(i, a, b) for (int i = (a); i < (b); i++)
+#define forl(i, a, b) for (long long i = (a); i < (b); i++)
+#define all(x) (x).begin(), (x).end()
+#define vmax(v) *max_element(all(v))
+#define vmin(v) *min_element(all(v))
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    fast_io();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vi a(n);
+        fori(i,0,n){
+            cin>>a[i];
+        }
+        fori(i,0,n){
+            if(a[i]==1) a[i]++;
+        }
+        fori(i,0,n-1){
+            if(a[i+1]%a[i]==0){
+                a[i+1]++;
+            }
+        }
+        fori(i,0,n){
+            cout<<a[i]<<" ";
+        }cout<<endl;
+    }
+}
