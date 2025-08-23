fa5e0f5e340fc914b23f264869bcd3628579aa7b
Kinjal2103 <kinjalagarwal2103@gmail.com>
2025-08-23 20:55:51 +0530

    Done Revisit

---
+++ 1679A.cpp
@@ -0,0 +1,41 @@
+//https://codeforces.com/problemset/problem/1679/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pb push_back
+#define pob pop_back
+#define fori(i, a, b) for (int i = (a); i < (b); i++)
+#define forl(i, a, b) for (long long i = (a); i < (b); i++)
+#define all(x) (x).begin(), (x).end()
+#define vmax(v) *max_element(all(v))
+#define vmin(v) *min_element(all(v))
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+int main() {
+    fast_io();
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        if(n<4 || n%2!=0) cout<<-1<<endl;
+        else {
+            ll min=ceil(n*1.0/6);
+            ll max=n/4;
+            cout<<min<<" "<<max<<endl;
+        }
+
+    }
+}
