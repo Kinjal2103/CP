37b3cdc6e6c8f8dde5cfc8efcf91a4cec437ca21
Kinjal2103 <kinjalagarwal2103@gmail.com>
2025-08-26 23:34:23 +0530

    Contest Understood

---
+++ 2133B.cpp
@@ -0,0 +1,50 @@
+//https://codeforces.com/contest/2133/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpii vector<pair<int,int>>
+#define pb push_back
+#define pob pop_back
+#define fori(i, a, b) for (int i = (a); i < (b); i++)
+#define forl(i, a, b) for (long long i = (a); i < (b); i++)
+#define all(x) (x).begin(), (x).end()
+#define vmax(v) *max_element(all(v))
+#define vmin(v) *min_element(all(v))
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+int main() {
+    fast_io();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vll g(n);
+        fori(i,0,n){
+            cin>>g[i];
+        }
+        sort(all(g));
+        ll ans=0;
+        if(n%2){
+            for(int i=0;i<n;i=i+2){
+                ans+=g[i];
+            }
+        }else{
+            for(int i=1;i<n;i=i+2){
+                ans+=g[i];
+            }
+        }
+        cout<<ans<<endl;
+    }
+}
